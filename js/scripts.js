f70f96bbbb3f00403fa3b8decbbbc1490b644dba
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-06-19 16:21:23 -0700

    change topping values to words, fix issue with values not populating to constructor function

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,15 @@
-// // business logic 
 
-// function Pizza (toppings, size) {
-//   this.toppings = toppings;
-//   this.size = size;
-// }
-
-// let pizza = new Pizza(toppings, size) 
-
-// Pizza.prototype.cost = function() {
-//   return this.toppings.length + (this.size)
-// }
-
-// // UI Logic 
-
-// $(document).ready(function() {
-//   $("form#pizza-order").submit(function(event) {
-//       event.preventDefault();
-//   $("input:checkbox[name=toppings]:checked").each(function() {
-//     const inputToppings = $(this).val();
-//     this.toppings = inputToppings;
-//     console.log(this.toppings)
-//     $("#pizza-specs").text(this.toppings);
-
-//   })
-// });
-// });
 
 let inputToppings = []
 let inputSize;
 let inputQuantity;
 let pizza = new Pizza(inputToppings, inputSize, inputQuantity)
 
-function Pizza (toppings, size, veganCrust, numberOfPizzas, totalPrice) {
-this.toppings = toppings;
+function Pizza (toppings, size, veganCrust, quantity, totalPrice) {
+this.toppings = [];
 this.size = size;
 this.veganCrust = veganCrust;
-this.numberOfPizzas = numberOfPizzas;
+this.quantity = quantity;
 this.totalPrice = totalPrice
 }
 
@@ -49,33 +23,22 @@ this.deliveryZip = deliveryZip;
 
 Pizza.prototype.addToppings = function() {
   $("input:checkbox[name=toppings]:checked").each(function(){
-    inputToppings.push($(this).val());
+    pizza.toppings.push($(this).val());
   });
 }
 
 
 
 Pizza.prototype.calculatePrice = function () {
-  let toppingsTotal = parseInt(toppings.indexOf(length-1))
-  let sizePrice;
-    if (size === 1) {
-      sizePrice = 10
-    } else if (size === 2) {
-      sizePrice = 15
-    } else if (size === 3) {
-      sizePrice = 20
-    } 
-    else {sizePrice = 0}
-  this.totalPrice += (toppingsTotal + sizePrice) * numberOfPizzas
+ if (pizza.size === 1) {
+   pizza.totalPrice = 15 * pizza.quantity
+ } else if (pizza.size === 2) {
+   pizza.totalPrice = 20 * pizza.quantity
+ } else {pizza.totalPrice = 25 * pizza.quantity}
 }
 
 DeliveryAddress.prototype.fullAddress = function () {
-  return this.deliveryStreetNumber + " " + this.deliveryStreetName + "<br>" + this.deliveryCity + ", " + this.deliveryState + " " + this.deliveryZip }
-
-Pizza.prototype.clearOrder = function () {
-  resetFields();
-  orderReceived();
-}
+  return "<br>" + this.deliveryStreetNumber + " " + this.deliveryStreetName + "<br>" + this.deliveryCity + ", " + this.deliveryState + " " + this.deliveryZip }
 
 // UI logic
 
@@ -85,21 +48,22 @@ $(document).ready(function() {
     event.preventDefault();
     pizza.addToppings();
     addressInput();
-    $("#pizza-specs").text(pizza.toppings);
+    sizeInput();
+    quantity();
+    $("#pizza-specs").text(pizza.calculatePrice());
     $("#orderConfirmation").text(address.fullAddress);
-    $("#order-confirmation").show();
-  });
-});
-  
-   let size = parseInt($("#size").val());
-  let quantity = parseInt($("#quantity").val());
+    $("#order-confirmation").show(); 
 
-//  function veganCrustInput() {
-//    if ($("input:radio[name=vegan-crust]:checked")) {
-//    let veganCrust = true;
-//    } else let veganCrust = false;
-//  }
 
+  function sizeInput() {
+  pizza.size = parseInt($("input:radio[name=size]:checked").val());
+  }
+
+  function quantity(){
+  pizza.quantity = parseInt($("input:text[name=quantity]").val());
+  }
+
+console.log(pizza)
  function addressInput() {
    let deliveryStreetNumber = $("#delivery-street-number").val();
    let deliveryStreetName = $("#delivery-street-name").val();
@@ -107,6 +71,7 @@ $(document).ready(function() {
    let deliveryState = $("#delivery-state").val();
    let deliveryZip = $("#delivery-zip").val();
    let address = new DeliveryAddress (deliveryStreetNumber, deliveryStreetName, deliveryCity, deliveryState, deliveryZip)
-   $("#delivery-address").text(address.fullAddress())
-   console.log(address.fullAddress())
+   $("#delivery-address").html(address.fullAddress())
  };
+});
+});
