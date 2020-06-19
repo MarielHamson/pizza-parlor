1e0ec057131657fa868bfaeaf91304ebc0a76bf1
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-06-19 09:32:49 -0700

    add prototypes for toppings, address, price, reset order fields. Build jQuery statements to obtain values

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,64 @@
-function AddressBook()
-this.contacts = []
+// business logic 
 
-function Contact (firstName, lastName, phoneNumber, emailAddress, pizzaOrder)
-this.firstName = firstName
-this.lastName = lastName
+function Pizza (toppings, size, crustType, veganCrust, numberOfPizzas, totalPrice)
+this.toppings = [];
+this.size = size;
+this.crustType = crustType;
+this.veganCrust = veganCrust;
+this.numberOfPizzas = numberOfPizzas;
+this.totalPrice = totalPrice
+
+function DeliveryAddress (deliveryStreetNumber, deliveryStreetName, deliveryCity, deliveryState, deliveryZip)
+this.deliveryStreetNumber = deliveryStreetNumber;
+this.deliveryStreetName = deliveryStreetName;
+this.deliveryCity = deliveryCity;
+this.deliveryState = deliveryState;
+this.deliveryZip = deliveryZip;
+
+Pizza.prototype.addToppings = function (topping) {
+  this.toppings().push(topping);
+}
+
+Pizza.prototype.calculatePrice = function () {
+  this.totalPrice += (toppingsTotal + sizePrice + crustTypePrice)
+}
+
+DeliveryAddress.prototype.fullAddress = function () {
+  this.deliveryStreetNumber + " " + this.deliveryStreetName + "<br>" + this.deliveryCity + ", " + this.deliveryState + " " + this.deliveryZip }
+
+Pizza.prototype.clearOrder = function () {
+  resetFields();
+  orderReceived();
+}
+
+// UI logic
+
+$(document).ready(function() {
+function toppingsInput(){
+$("input:checkbox[name=toppings]:checked").forEach(function() {
+  toppings.push($(this).val())}
+)};
+
+function sizeInput() {
+  let size = $("input:radio[name=size]:checked");
+}
+
+function crustInput() {
+  let crustType = $("input:radio[name=crust]:checked");
+}
+
+function addressInput() {
+  let deliveryStreetNumber = $("#delivery-street-number".val());
+  let deliveryStreetName = $("#delivery-street".val());
+  let deliveryCity = $("#delivery-city".val());
+  let deliveryState = $("#delivery-state".val());
+  let deliveryZip = $("#delivery-zip".val());
+  let address = new DeliveryAddress (deliveryStreetNumber, deliveryStreetName, deliveryCity, deliveryState, deliveryZip)
+  $("#address-confirmation".text(address.fullAddress))
+};
+
+
+
+
+
+});
