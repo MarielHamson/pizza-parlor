85dd8b1f86289f583a6f9e48bffd07d7aeee4fc1
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-06-24 19:48:21 -0700

    fix indentation

--- js/scripts.js
+++ js/scripts.js
@@ -12,20 +12,20 @@ class Pizza {
       this.totalPrice = 15 * this.quantity
     } else if (this.size === 2) {
       this.totalPrice = 20 * this.quantity
-    } else if (this.size === 3){
+    } else if (this.size === 3) {
       this.totalPrice = 25 * this.quantity
-    } 
+    }
   }
 };
 
 class DeliveryAddress {
-  constructor (deliveryStreetNumber, deliveryStreetName, deliveryCity, deliveryState, deliveryZip) {
-  this.deliveryStreetNumber = deliveryStreetNumber;
-  this.deliveryStreetName = deliveryStreetName;
-  this.deliveryCity = deliveryCity;
-  this.deliveryState = deliveryState;
-  this.deliveryZip = deliveryZip;
-}
+  constructor(deliveryStreetNumber, deliveryStreetName, deliveryCity, deliveryState, deliveryZip) {
+    this.deliveryStreetNumber = deliveryStreetNumber;
+    this.deliveryStreetName = deliveryStreetName;
+    this.deliveryCity = deliveryCity;
+    this.deliveryState = deliveryState;
+    this.deliveryZip = deliveryZip;
+  }
 
   fullAddress() {
     return `<br> ${this.deliveryStreetNumber} ${this.deliveryStreetName} <br> ${this.deliveryCity}, ${this.deliveryState} ${this.deliveryZip}`
@@ -51,16 +51,16 @@ $(document).ready(function() {
     $("input:checkbox[name=toppings]:checked").each(function() {
       let toppings = $(this).val();
       inputToppings.push(toppings)
-    let inputSize = parseInt($("input:radio[name=size]:checked").val());
-    let inputQuantity = parseInt($("input:text[name=quantity]").val());
-    let veganCrust = $("input:radio[name=vegan-crust]:checked").val();
-    let totalPrice;
-    let pizza = new Pizza(inputToppings, inputSize, veganCrust, inputQuantity, totalPrice)
-    addressInput();
-    pizza.calculatePrice();
-    $("#pizza-specs").html(`<br> Your pizza order total is $ ${pizza.totalPrice}. <br> Your pizza toppings are: ${pizza.toppings}. <br> Your crust type is: ${pizza.veganCrust} <br> You ordered ${pizza.quantity} pizzas`);
-    $("#orderConfirmation").text(address.fullAddress);
-    $("#order-confirmation").show();
-    });
+      let inputSize = parseInt($("input:radio[name=size]:checked").val());
+      let inputQuantity = parseInt($("input:text[name=quantity]").val());
+      let veganCrust = $("input:radio[name=vegan-crust]:checked").val();
+      let totalPrice;
+      let pizza = new Pizza(inputToppings, inputSize, veganCrust, inputQuantity, totalPrice)
+      addressInput();
+      pizza.calculatePrice();
+      $("#pizza-specs").html(`<br> Your pizza order total is $ ${pizza.totalPrice}. <br> Your pizza toppings are: ${pizza.toppings}. <br> Your crust type is: ${pizza.veganCrust} <br> You ordered ${pizza.quantity} pizzas`);
+      $("#orderConfirmation").text(address.fullAddress);
+      $("#order-confirmation").show();
     });
   });
+});
